c8aa5e56f1936ae241cd66bc7818e31edd0184f2
Mikel Diaz <85890651+SBolivarLoL@users.noreply.github.com>
2022-07-09 22:37:03 -0500

    modified response to /api/diaries

--- src/services/diary.ts
+++ src/services/diary.ts
@@ -1,8 +1,12 @@
 import diaryData from './diaries.json'
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
 
 const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({ id, date, weather, visibility }))
+}
 
 export const addEntry = (): undefined => undefined
--- src/types.d.ts
+++ src/types.d.ts
@@ -8,3 +8,15 @@ export interface DiaryEntry {
   visibility: Visibility
   comment: string
 }
+
+/**
+ * Los dos tipos de datos abajo son los más utiles de typescript
+ * porque nos permiten omitir informacion sensible sin tener que
+ * duplicar tipos
+ *
+ * Con uno seleccionamos los datos que queremos
+ * Con el otro seleccionamos los datos que no queremos
+ */
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
